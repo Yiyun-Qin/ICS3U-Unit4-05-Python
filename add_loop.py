2642b9fbc6d5a84e095bef67e5b6d43d3b49a54d
Yiyun Qin <yiyun.qin@mths.ca>
2022-04-26 18:43:09 +0000

    add loop

--- add_loop.py
+++ add_loop.py
@@ -2,29 +2,34 @@
 
 # Created by Yiyun Qin
 # Created in April 2022
-# This is the math program, adding number from 1 to n
+# This is the math program, adding numbers input by the user
 
 
 def main():
-    # This function adds number from 1 to number the user enters
+    # This function adds numbers from the user
 
     # input
-    add_string = input("Enter a positive integer: ")
+    add_time_string = input("How many numbers are you going to add: ")
 
     # process & output
-    loop_counter = 0
     answer = 0
     print("")
     try:
-        add_integer = int(add_string)
-        while loop_counter <= add_integer:
-            answer = answer + loop_counter
-            loop_counter = loop_counter + 1
-        print(
-            "The sum of all positive integers from 1 to {0} is {1}.".format(
-                add_string, answer
-            )
-        )
+        add_time_integer = int(add_time_string)
+        for counter in range(add_time_integer):
+            add_number_string = input("Enter a number to add: ")
+            try:
+                add_number_integer = int(add_number_string)
+                if add_number_integer >= 0:
+                    answer = answer + add_number_integer
+                else:
+                    answer = answer + 0
+                if counter < add_time_integer - 1:
+                    continue
+                else:
+                    print("The sum of just positive numbers is {}.".format(answer))
+            except Exception:
+                print("Invalid number!")
     except Exception:
         print("Invalid number!")
     finally:
