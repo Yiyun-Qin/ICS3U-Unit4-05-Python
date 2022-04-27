a6ba1ca45b3e65489b10ad75f6332c9e93d1f492
Yiyun Qin <yiyun.qin@mths.ca>
2022-04-27 01:11:58 +0000

    add loop

--- add_loop.py
+++ add_loop.py
@@ -16,20 +16,23 @@ def main():
     print("")
     try:
         add_time_integer = int(add_time_string)
-        for counter in range(add_time_integer):
-            add_number_string = input("Enter a number to add: ")
-            try:
-                add_number_integer = int(add_number_string)
-                if add_number_integer >= 0:
-                    answer = answer + add_number_integer
-                else:
-                    answer = answer + 0
-                if counter < add_time_integer - 1:
-                    continue
-                else:
-                    print("The sum of just positive numbers is {}.".format(answer))
-            except Exception:
-                print("Invalid number!")
+        if add_time_integer <= 0:
+            print("Please put in a positive integer.")
+        else:
+            for counter in range(add_time_integer):
+                add_number_string = input("Enter a number to add: ")
+                try:
+                    add_number_integer = int(add_number_string)
+                    if add_number_integer >= 0:
+                        answer = answer + add_number_integer
+                    else:
+                        answer = answer + 0
+                    if counter < add_time_integer - 1:
+                        continue
+                    else:
+                        print("The sum of just the positive numbers is {}.".format(answer))
+                except Exception:
+                    print("Invalid number!")
     except Exception:
         print("Invalid number!")
     finally:
